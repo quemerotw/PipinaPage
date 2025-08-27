ae2e774e47126439d045ac8a390e30092a81e3ff
quemerotw <quemerotw@localhost.localdomain>
2025-08-27 20:25:41 -0300

    inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+  if (navbar.style.left === "0px") {
+    navbar.style.left = "-100%";
+  } else {
+    navbar.style.left = "0px";
+  }
+});
+
+
+
+
+
+class popupPizza {
+    constructor(parameters) {
+        
+    }
+}
